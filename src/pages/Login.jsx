9f5e9815a549896d09887d0eeddbb895844493d3
Kaveen LK <kmklaksitha@gmail.com>
2022-11-01 17:13:52 +0530

    updated the login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
 export const Login = () => {
+
+    //login method
+    const signin = (e) => {
+        e.preventDefault();
+
+    }
     return (
-        <div>Login</div>
+        <div className='login-body'>
+            <div id="loginform">
+                <h2 id="headerTitle">Login</h2>
+                <form>
+                    <div class="row">
+                        <label>Username</label>
+                        <input type="text" placeholder="Enter your username" />
+                    </div>
+                    <div class="row">
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter your password" />
+                    </div>
+                    <div id="button" class="row">
+                        <button onClick={(e) => { signin(e) }}>
+                            Signin
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
