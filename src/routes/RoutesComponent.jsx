da1020fd922daa722dbda6e56ef17f4d138afeca
Kaveen LK <kmklaksitha@gmail.com>
2022-11-05 13:09:05 +0530

    updated firebase component

--- src/routes/RoutesComponent.jsx
+++ src/routes/RoutesComponent.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Login } from '../pages/Login'
 import Register from '../pages/Register'
 import CreateMessage from '../pages/createMsg'
+import { uploadFile as UploadFile } from '../pages/uploadFile'
+
 // import Footer from '../components/Footer'
 // import { Navigationbar } from '../components/Navigationbar'
 // import { CreateTemplate } from '../pages/admin/CreateTemplate'
@@ -19,6 +21,7 @@ export const RoutesComponent = () => {
                     <Route exact path='/login' element={<Login />} />
                     <Route exact path='/register' element={<Register />} />
                     <Route exact path='/message' element={<CreateMessage />} />
+                    <Route exact path='/file' element={<UploadFile />} />
                 </Routes>
                 {/* <Footer /> */}
             </Router>
