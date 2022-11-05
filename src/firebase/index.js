4146fe86c94e1a5991eb63b0ee4aa646890b6b52
Kaveen LK <kmklaksitha@gmail.com>
2022-11-05 12:43:49 +0530

    downgraded react to v17

---
+++ src/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getStorage } from "firebase/storage";
+import "firebase/storage";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDj6eGjZTnV8LdkIOWC88ZzsFHOe22tiv0",
+    authDomain: "fir-storage-private.firebaseapp.com",
+    projectId: "fir-storage-private",
+    storageBucket: "fir-storage-private.appspot.com",
+    messagingSenderId: "641762296362",
+    appId: "1:641762296362:web:e509c575dc373665037953"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app);
+
+export { storage, firebase as default };
