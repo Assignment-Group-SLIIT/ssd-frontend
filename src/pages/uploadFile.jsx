a7aafa38348f46e0ee8244300de17e5dbcb771dd
Kaveen LK <kmklaksitha@gmail.com>
2022-11-06 16:24:23 +0530

    upload file connected with API

--- src/pages/uploadFile.jsx
+++ src/pages/uploadFile.jsx
@@ -1,47 +1,33 @@
+import '../styles/upload.styles.scss'
 import React, { useEffect, useState } from 'react'
 import DropzoneArea from '../components/DropZoneArea'
-import { Form, Row, Col, ProgressBar } from 'react-bootstrap'
+import { ProgressBar } from 'react-bootstrap'
 import toastNotification from '../components/toastNotification';
-import '../styles/upload.styles.scss'
+import { uploadFile } from '../services/filesupload.service';
 
 
 export const UploadFile = () => {
     const [fileName, setFileName] = useState({ value: "", error: "Please select a file", isError: false });
     const [loadingProgress, setLoadingProgress] = useState(10)
-    const [state, setState] = useState(false)
+    const [state, setState] = useState(false);
+    const [email, setEmail] = useState("");
 
     useEffect(() => {
         fileName.value === "" ? setFileName({ ...fileName, isError: true }) : setFileName({ ...fileName, isError: false })
     }, [fileName.value])
 
     const sendProgress = (data) => {
-        console.log("DATAAAA", data)
         setState(true)
         setLoadingProgress(data)
     }
 
-    // useEffect(() => {
-    //     const user = JSON.parse(sessionStorage.getItem("user"));
-
-    //     if (user.groupId != "") {
-    //         setGroupId(user?.groupId)
-    //         getOneGroup(user?.groupId).then((res) => {
-    //             console.log("res>>", res.data)
-    //             if (res.ok) {
-    //                 setTopic(res?.data?.researchTopic)
-    //                 setField(res?.data?.researchField);
-    //             } else {
-    //                 console.log("error while getting group data")
-    //             }
-    //         }).catch((err) => {
-    //             console.log("error while getting group data", err.error)
-    //         })
-    //     }
-    // }, [])
+    useEffect(() => {
+        const user = JSON.parse(sessionStorage.getItem("user"));
+        setEmail(user?.email);
+    }, [])
 
 
     const sendData = (data) => {
-        console.log("Child data", data)
         setFileName({ ...fileName, value: data })
     }
 
@@ -49,22 +35,21 @@ export const UploadFile = () => {
         e.preventDefault()
 
         const payload = {
-            document: fileName.value
+            fileLink: fileName.value,
+            email
         }
-        // if (!email.isError && !fileName.isError) {
-        //     // console.log("payload>>", payload)
-        //     createProjectProposal(payload).then((res) => {
-        //         if (res.ok) {
-        //             toastNotification("Project proposal has been submitted successfully!", "success")
-        //             window.location.reload()
-        //         } else {
-        //             toastNotification("Error occured!", "error")
-        //         }
-        //     }).catch((err) => {
-        //         toastNotification("Error occured!", "error")
-        //         console.log("error while registering>>", err.err)
-        //     })
-        // }
+        if (!fileName.isError) {
+            uploadFile(payload).then((res) => {
+                if (res.ok) {
+                    toastNotification("File has been uploaded successfully!", "success")
+                    window.location.reload();
+                } else {
+                    toastNotification("Error occured!", "error")
+                }
+            }).catch((err) => {
+                toastNotification("Error occured!", "error")
+            })
+        }
 
     }
 
