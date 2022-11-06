78a008d92baa507dda8b051e0d231e99db71ef6a
Thisara Thathsarani <thisararpg@gmail.com>
2022-11-06 13:47:02 +0530

    login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { loginUser } from '../services/user.service';
 import NavigationBar from './navBar';
+import { useNavigate } from 'react-router-dom';
+import toastNotification from "../components/toastNotification"
 
 export const Login = () => {
+
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -10,6 +14,7 @@ export const Login = () => {
     //login method
     const signin = (e) => {
         e.preventDefault();
+
         const payload = {
             email,
             password
@@ -17,9 +22,12 @@ export const Login = () => {
 
         loginUser(payload).then((res) => {
             console.log("response" , res)
+            res.ok ? toastNotification("Success!", "success") : toastNotification("Email or Password is incorrect!", "error")
+
             const user = JSON.parse(sessionStorage.getItem("user"))
-            if(user.type === "Worker"){
+            if(user.type === "worker"){
                 sessionStorage.setItem("type" , "Worker")
+                navigate("/message");
             }else{
                 sessionStorage.setItem("type" , "Manager")
             }
--- src/pages/createMsg.jsx
+++ src/pages/createMsg.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 import NavigationBar from './navBar'
+import { createMessage } from '../services/message.service';
+import toastNotification from "../components/toastNotification"
+import { useEffect } from 'react';
 
 const CreateMessage = () => {
 
-    const createMessage = () => {
+    const [email, setEmail] = useState("");
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        //console.log("sessionStorage " , sessionStorage.user);
+        //console.log("sessionStorage " , email);
+    })
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        const user = JSON.parse(sessionStorage.getItem("user"))
+
+        const payload = {
+            email:user.email,
+            description
+        }
+
+        createMessage(payload).then((res) => {
+            console.log("response" , res)
+            res.ok ? toastNotification("Success!", "success") : toastNotification("Email or Message is incorrect!", "error")
+
+
+          
+        }).catch((err) => {
+            console.log("error while sign in >>", err.ok)
+
+        })
 
     }
 
@@ -17,12 +47,14 @@ const CreateMessage = () => {
                         <div class="row">
                             <label>Message</label>
                             <textarea
-
-                                class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
+                                class="form-control" 
+                                onChange={e => { setDescription(e.target.value); }}
+                                id="exampleFormControlTextarea1" 
+                                rows="7"></textarea>
                         </div>
 
                         <div id="button" class="row">
-                            <button onClick={(e) => { createMessage(e) }}>
+                            <button onClick={(e) => { sendMessage(e) }}>
                                 Submit
                             </button>
                         </div>
